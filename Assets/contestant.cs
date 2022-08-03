319c7e91a2b1ddd125c17ddf48d1ef1fe8dd7a91
tasthing <lufarionite@gmail.com>
2022-08-02 20:45:28 -0400

    Solve animation

---
+++ Assets/contestant.cs
@@ -0,0 +1,11 @@
+﻿public partial class objectShows
+{
+    private class contestant
+    {
+        public int id;
+        public int pos;
+        public int[] scores;
+        public int appeal;
+        public char snchar;
+    }
+}
--- Assets/objectShows.cs
+++ Assets/objectShows.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using KModkit;
 using rnd = UnityEngine.Random;
 
-public class objectShows : MonoBehaviour
+public partial class objectShows : MonoBehaviour
 {
     public new KMAudio audio;
     public KMBombInfo bomb;
@@ -19,57 +19,58 @@ public class objectShows : MonoBehaviour
     public Texture[] charactermats;
     public Texture winner;
     public Renderer contestname;
+    public Transform dummy;
 
-    private static readonly string[] contestnames = new string[16] { "wipeout", "underwater basket weaving", "water balloon fight", "cave diving", "chariot race", "equestrian acrobatics", "gladiatorial fight", "the objective games", "escape the volcano", "jungle survival", "tiger taming", "cliff climbing", "sack race", "interpretive dance", "nose nabbing", "calvinball" };
-    private static readonly string[] ordinals = new string[6] { "first", "second", "third", "fourth", "fifth", "sixth" };
-    private static readonly string[] placementordinals = new string[6] { "last", "fifth", "fourth", "third", "second", "first" };
-    private static readonly string[] charlists = new string[4] { "KIO68QU9ZCPDSJMEVRAT1X53B427HLG0YFWN", "CYXD7SVI0NUTLJMQOHERF45G2986P31KWZAB", "BMVF31QZ0Y4SXJ5GIW7H6A2EPRLNTKUDC98O", "MWC509QI31NOSJB2FHUDXZ6PLV7TYK8G4ERA" };
-    public string[] charnames;
+    private static readonly string[] contestNames = new[] { "wipeout", "underwater basket weaving", "water balloon fight", "cave diving", "chariot race", "equestrian acrobatics", "gladiatorial fight", "the objective games", "escape the volcano", "jungle survival", "tiger taming", "cliff climbing", "sack race", "interpretive dance", "nose nabbing", "calvinball" };
+    private static readonly string[] ordinals = new[] { "first", "second", "third", "fourth", "fifth", "sixth" };
+    private static readonly string[] placementOrdinals = new[] { "last", "fifth", "fourth", "third", "second", "first" };
+    private static readonly string[] contestStrings = new[] { "KIO68QU9ZCPDSJMEVRAT1X53B427HLG0YFWN", "CYXD7SVI0NUTLJMQOHERF45G2986P31KWZAB", "BMVF31QZ0Y4SXJ5GIW7H6A2EPRLNTKUDC98O", "MWC509QI31NOSJB2FHUDXZ6PLV7TYK8G4ERA" };
+    private static readonly string[] characterNames = new[] { "Battleship", "Beer", "Big Circle", "Black Hole", "Block", "Bulb", "Calendar", "Clock", "Combination Lock", "Cookie Jar", "Domino", "Fidget Spinner", "Hypercube", "Ice Cream", "iPhone", "Jack O' Lantern", "Lego", "Moon", "Necronomicon", "Paint Brush", "Radio", "Resistor", "Rubik's Clock", "Rubik's Cube", "Snooker Ball", "Sphere", "Sticky Note", "Stopwatch", "Sun", "Tennis Racket" };
 
-    private int startingtime;
-    private int startingday;
+    private int startingTime;
+    private int startingDay;
     private int stage;
-    private Contestant[] solution;
-    private int[] publicappeals = new int[30];
-    private List<Contestant> chosencharacters = new List<Contestant>();
+    private contestant[] solution;
+    private int[] publicAppeals = new int[30];
+    private List<contestant> contestantsPresent = new List<contestant>();
     private List<int> contests = new List<int>();
-    private List<int> unpressedbuttons;
+    private List<int> unpressedButtons;
 
     private static int moduleIdCounter = 1;
     private int moduleId;
     private bool moduleSolved;
 
-    void Awake()
+    private void Awake()
     {
         moduleId = moduleIdCounter++;
         foreach (KMSelectable button in buttons)
             button.OnInteract += delegate () { buttonPress(button); return false; };
     }
 
-    void Start()
+    private void Start()
     {
-        startingtime = (int)bomb.GetTime();
-        startingday = (int)DateTime.Now.DayOfWeek;
-        startingday++;
+        startingTime = (int)bomb.GetTime();
+        startingDay = (int)DateTime.Now.DayOfWeek;
+        startingDay++;
         Reset();
     }
 
-    void Reset()
+    private void Reset()
     {
-        chosencharacters.Clear();
+        contestantsPresent.Clear();
         contests.Clear();
         for (int i = 0; i < 6; i++)
             buttons[i].gameObject.SetActive(true);
         contestname.gameObject.SetActive(true);
         stage = 0;
-        unpressedbuttons = Enumerable.Range(0, 6).ToList();
+        unpressedButtons = Enumerable.Range(0, 6).ToList();
         GetAppeals();
         PickCharacters();
         GetSolution();
         contestname.material.mainTexture = contesttextures[contests[0]];
     }
 
-    void PickCharacters()
+    private void PickCharacters()
     {
         var ser = bomb.GetSerialNumber().ToCharArray();
         var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -77,34 +78,25 @@ public class objectShows : MonoBehaviour
             while (ser.Take(i).Contains(ser[i]))
                 ser[i] = alphabet[(alphabet.IndexOf(ser[i]) + 1) % 36];
         int index;
-        chosencharacters.Clear();
+        contestantsPresent.Clear();
         for (int i = 0; i < 6; i++)
         {
-            index = UnityEngine.Random.Range(0, 30);
-            while (chosencharacters.Any(chr => chr.id == index))
-                index = UnityEngine.Random.Range(0, 30);
-            var character = new Contestant { id = index, pos = i, scores = new int[5], appeal = publicappeals[index], snchar = ser[i] };
-            while (chosencharacters.Any(chr => chr.appeal == character.appeal))
+            index = rnd.Range(0, 30);
+            while (contestantsPresent.Any(chr => chr.id == index))
+                index = rnd.Range(0, 30);
+            var character = new contestant { id = index, pos = i, scores = new int[5], appeal = publicAppeals[index], snchar = ser[i] };
+            while (contestantsPresent.Any(chr => chr.appeal == character.appeal))
                 character.appeal++;
-            chosencharacters.Add(character);
+            contestantsPresent.Add(character);
             buttonrenders[i].material.mainTexture = charactermats[index];
-            Debug.LogFormat("[Object Shows #{0}] the {2} character is {1}, who has a public appeal of {3}.", moduleId, charnames[index], ordinals[i], character.appeal);
+            Debug.LogFormat("[Object Shows #{0}] the {2} character is {1}, who has a public appeal of {3}.", moduleId, characterNames[index], ordinals[i], character.appeal);
         }
     }
 
-    class Contestant
+    private void GetSolution()
     {
-        public int id;
-        public int pos;
-        public int[] scores;
-        public int appeal;
-        public char snchar;
-    }
-
-    void GetSolution()
-    {
-        var currentcharacters = chosencharacters.ToList();
-        solution = new Contestant[5];
+        var currentcharacters = contestantsPresent.ToList();
+        solution = new contestant[5];
         var contestindices = Enumerable.Range(0, 16).ToList().Shuffle();
         for (int i = 0; i < 5; i++)
         {
@@ -113,27 +105,27 @@ public class objectShows : MonoBehaviour
             int styleindex = contestindex / 4;
             if (i != 4)
             {
-                Debug.LogFormat("[Object Shows #{0}] the {2} contest is {1}.", moduleId, contestnames[contestindex], ordinals[i]);
+                Debug.LogFormat("[Object Shows #{0}] the {2} contest is {1}.", moduleId, contestNames[contestindex], ordinals[i]);
                 contests.Add(contestindex);
             }
             for (int j = 0; j < currentcharacters.Count; j++)
             {
                 if (styleindex == 2 || styleindex == 3)
-                    currentcharacters[j].scores[i] = 35 - charlists[typeindex].IndexOf(currentcharacters[j].snchar);
+                    currentcharacters[j].scores[i] = 35 - contestStrings[typeindex].IndexOf(currentcharacters[j].snchar);
                 else
-                    currentcharacters[j].scores[i] = charlists[typeindex].IndexOf(currentcharacters[j].snchar);
+                    currentcharacters[j].scores[i] = contestStrings[typeindex].IndexOf(currentcharacters[j].snchar);
             }
             var sortedcharacters = currentcharacters.OrderBy(chr => chr.scores[i]).ToList();
             if (i < 3)
             {
                 var lowestappeal = sortedcharacters.Take(i == 0 ? 3 : 2).Min(chr => chr.appeal);
-                Debug.LogFormat("[Object Shows #{0}] The scores are: {1}.", moduleId, sortedcharacters.Select(chr => charnames[chr.id] + " = " + chr.scores[i]).Join(", "));
+                Debug.LogFormat("[Object Shows #{0}] The scores are: {1}.", moduleId, sortedcharacters.Select(chr => characterNames[chr.id] + " = " + chr.scores[i]).Join(", "));
                 solution[i] = currentcharacters.First(chr => chr.appeal == lowestappeal);
             }
             else if (i == 3)
             {
                 solution[i] = sortedcharacters[0];
-                Debug.LogFormat("[Object Shows #{0}] The scores are: {1}.", moduleId, sortedcharacters.Select(chr => charnames[chr.id] + " = " + chr.scores[i]).Join(", "));
+                Debug.LogFormat("[Object Shows #{0}] The scores are: {1}.", moduleId, sortedcharacters.Select(chr => characterNames[chr.id] + " = " + chr.scores[i]).Join(", "));
             }
             else
             {
@@ -141,16 +133,16 @@ public class objectShows : MonoBehaviour
                 solution[i] = currentcharacters.First(chr => chr.appeal == lowestappeal);
             }
             currentcharacters.Remove(solution[i]);
-            Debug.LogFormat("[Object Shows #{0}] the character in {2} place is {1}.", moduleId, charnames[solution[i].id], placementordinals[i]);
+            Debug.LogFormat("[Object Shows #{0}] the character in {2} place is {1}.", moduleId, characterNames[solution[i].id], placementOrdinals[i]);
         }
     }
 
-    void buttonPress(KMSelectable button)
+    private void buttonPress(KMSelectable button)
     {
         if (!moduleSolved)
         {
             button.AddInteractionPunch(.5f);
-            if (chosencharacters[Array.IndexOf(buttons, button)] != solution[stage])
+            if (contestantsPresent[Array.IndexOf(buttons, button)] != solution[stage])
             {
                 module.HandleStrike();
                 audio.PlaySoundAtTransform("strike" + rnd.Range(1, 3), transform);
@@ -166,65 +158,87 @@ public class objectShows : MonoBehaviour
                     moduleSolved = true;
                     module.HandlePass();
                     audio.PlaySoundAtTransform("solve1", transform);
-                    Debug.LogFormat("[Object Shows #{0}] Module solved.", moduleId);
+                    Debug.LogFormat("[Object Shows #{0}] Module solved!", moduleId);
+                    StartCoroutine(SolveAnimation());
                 }
                 else if (stage == 4)
                 {
                     contestname.gameObject.SetActive(false);
                     audio.PlaySoundAtTransform("elimination", button.transform);
-                    unpressedbuttons.Remove(Array.IndexOf(buttons, button));
+                    unpressedButtons.Remove(Array.IndexOf(buttons, button));
                 }
                 else
                 {
                     contestname.material.mainTexture = contesttextures[contests[stage]];
                     audio.PlaySoundAtTransform("elimination", button.transform);
-                    unpressedbuttons.Remove(Array.IndexOf(buttons, button));
+                    unpressedButtons.Remove(Array.IndexOf(buttons, button));
                 }
             }
         }
     }
 
-    void GetAppeals()
+    private void GetAppeals()
     {
         var ser = bomb.GetSerialNumber();
-        publicappeals[0] = (bomb.GetBatteryCount() + bomb.GetIndicators().Count()) % 7; // Battleship
-        publicappeals[1] = bomb.GetModuleNames().Count() - bomb.GetSolvableModuleNames().Count(); // Beer
-        publicappeals[2] = bomb.GetPortCount(Port.Parallel) + bomb.GetPortCount(Port.DVI); // Big Circle
-        publicappeals[3] = ser[2] - '0' + ser[5] - '0'; // Black Hole
-        publicappeals[4] = bomb.GetPortCount(Port.Serial); // Block
-        publicappeals[5] = bomb.GetIndicators().Count(); // Bulb
-        publicappeals[6] = ((bomb.GetSerialNumberNumbers().Sum() - 1) % 9) + 1; // Calendar
-        publicappeals[7] = bomb.GetPortCount(Port.Serial) + bomb.GetPortCount(Port.Parallel); // Clock
-        publicappeals[8] = bomb.GetTwoFactorCounts(); // Combination Lock
-        publicappeals[9] = bomb.GetModuleNames().Count() % 10; // Cookie Jar
-        publicappeals[10] = bomb.GetOnIndicators().Count(); // Domino
-        publicappeals[11] = startingtime / 60; // Fidget Spinner
-        publicappeals[12] = bomb.GetBatteryCount(); // Hypercube
-        publicappeals[13] = ser[5] - '0'; // Ice Cream
-        publicappeals[14] = bomb.GetBatteryHolderCount(); // iPhone
-        publicappeals[15] = bomb.GetPortPlates().Count(); // Jack O' Lantern
-        publicappeals[16] = bomb.GetOffIndicators().Count(); // Lego
-        publicappeals[17] = bomb.GetPortCount(Port.PS2); // Moon
-        publicappeals[18] = (ser[4] - 'A' + 1) % 10; // Necronomicon
-        publicappeals[19] = bomb.GetIndicators().Count(ind => ser.Intersect(ind).Any()); // Paint Brush
-        publicappeals[20] = bomb.GetBatteryCount(Battery.AA); // Radio
-        publicappeals[21] = bomb.GetSerialNumberNumbers().Sum(); // Resistor
-        publicappeals[22] = (bomb.GetSerialNumberLetters().Select(let => let - 'A' + 1).Sum() - 1) % 9 + 1; // Rubik's Clock
-        publicappeals[23] = bomb.GetPortCount(Port.RJ45); // Rubik's Cube
-        publicappeals[24] = bomb.GetModuleNames().Count(mdl => new string[] { "color", "colour", "colo(u)r" }.Any(s => mdl.ContainsIgnoreCase(s))); // Snooker Ball
-        publicappeals[25] = bomb.GetBatteryCount(Battery.D); // Sphere
-        publicappeals[26] = startingday; // Sticky Note
-        publicappeals[27] = ((startingtime - 1) % 9) + 1; // Stopwatch
-        publicappeals[28] = bomb.GetModuleNames().Count(mdl => mdl.ContainsIgnoreCase("simon") || mdl.ContainsIgnoreCase("maze") || mdl.ContainsIgnoreCase("morse")); // Sun
-        publicappeals[29] = (ser[3] - 'A' + 1) % 10; // Tennis Racket
+        publicAppeals[0] = (bomb.GetBatteryCount() + bomb.GetIndicators().Count()) % 7; // Battleship
+        publicAppeals[1] = bomb.GetModuleNames().Count() - bomb.GetSolvableModuleNames().Count(); // Beer
+        publicAppeals[2] = bomb.GetPortCount(Port.Parallel) + bomb.GetPortCount(Port.DVI); // Big Circle
+        publicAppeals[3] = ser[2] - '0' + ser[5] - '0'; // Black Hole
+        publicAppeals[4] = bomb.GetPortCount(Port.Serial); // Block
+        publicAppeals[5] = bomb.GetIndicators().Count(); // Bulb
+        publicAppeals[6] = ((bomb.GetSerialNumberNumbers().Sum() - 1) % 9) + 1; // Calendar
+        publicAppeals[7] = bomb.GetPortCount(Port.Serial) + bomb.GetPortCount(Port.Parallel); // Clock
+        publicAppeals[8] = bomb.GetTwoFactorCounts(); // Combination Lock
+        publicAppeals[9] = bomb.GetModuleNames().Count() % 10; // Cookie Jar
+        publicAppeals[10] = bomb.GetOnIndicators().Count(); // Domino
+        publicAppeals[11] = startingTime / 60; // Fidget Spinner
+        publicAppeals[12] = bomb.GetBatteryCount(); // Hypercube
+        publicAppeals[13] = ser[5] - '0'; // Ice Cream
+        publicAppeals[14] = bomb.GetBatteryHolderCount(); // iPhone
+        publicAppeals[15] = bomb.GetPortPlates().Count(); // Jack O' Lantern
+        publicAppeals[16] = bomb.GetOffIndicators().Count(); // Lego
+        publicAppeals[17] = bomb.GetPortCount(Port.PS2); // Moon
+        publicAppeals[18] = (ser[4] - 'A' + 1) % 10; // Necronomicon
+        publicAppeals[19] = bomb.GetIndicators().Count(ind => ser.Intersect(ind).Any()); // Paint Brush
+        publicAppeals[20] = bomb.GetBatteryCount(Battery.AA); // Radio
+        publicAppeals[21] = bomb.GetSerialNumberNumbers().Sum(); // Resistor
+        publicAppeals[22] = (bomb.GetSerialNumberLetters().Select(let => let - 'A' + 1).Sum() - 1) % 9 + 1; // Rubik's Clock
+        publicAppeals[23] = bomb.GetPortCount(Port.RJ45); // Rubik's Cube
+        publicAppeals[24] = bomb.GetModuleNames().Count(mdl => new string[] { "color", "colour", "colo(u)r" }.Any(s => mdl.ContainsIgnoreCase(s))); // Snooker Ball
+        publicAppeals[25] = bomb.GetBatteryCount(Battery.D); // Sphere
+        publicAppeals[26] = startingDay; // Sticky Note
+        publicAppeals[27] = ((startingTime - 1) % 9) + 1; // Stopwatch
+        publicAppeals[28] = bomb.GetModuleNames().Count(mdl => mdl.ContainsIgnoreCase("simon") || mdl.ContainsIgnoreCase("maze") || mdl.ContainsIgnoreCase("morse")); // Sun
+        publicAppeals[29] = (ser[3] - 'A' + 1) % 10; // Tennis Racket
+    }
+
+    private IEnumerator SolveAnimation()
+    {
+        var elapsed = 0f;
+        var duration = .75f;
+        var finalButton = buttons[Enumerable.Range(0, 6).Where(x => !solution.Select(chr => chr.pos).ToArray().Contains(x)).First()].transform;
+        finalButton.SetParent(dummy);
+        var startPosition = finalButton.localPosition;
+        var finalPosition = new Vector3(0f, .0101f, 0f);
+        var startScale = finalButton.localScale;
+        var finalScale = new Vector3(.12f, .12f, 1f);
+        while (elapsed < duration)
+        {
+            finalButton.localPosition = new Vector3(Mathf.Lerp(startPosition.x, 0f, elapsed / duration), Mathf.Lerp(startPosition.y, .0101f, elapsed / duration), Mathf.Lerp(startPosition.z, 0f, elapsed / duration));
+            finalButton.localScale = new Vector3(Mathf.Lerp(startScale.x, .12f, elapsed / duration), Mathf.Lerp(startScale.y, .12f, elapsed / duration), 1f);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        finalButton.localPosition = finalPosition;
+        finalButton.localScale = finalScale;
     }
 
     // Twitch Plays
-    #pragma warning disable 414
+#pragma warning disable 414
     private readonly string TwitchHelpMessage = @"!{0} press <pos> [Presses the specified object in position 'pos'] | Valid object positions are tl, tr, ml, mr, bl, br";
-    #pragma warning restore 414
+#pragma warning restore 414
 
-    IEnumerator ProcessTwitchCommand(string command)
+    private IEnumerator ProcessTwitchCommand(string command)
     {
         string[] parameters = command.Split(' ');
         if (Regex.IsMatch(parameters[0], @"^\s*press\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
@@ -273,9 +287,9 @@ public class objectShows : MonoBehaviour
         }
     }
 
-    IEnumerator TwitchHandleForcedSolve()
+    private IEnumerator TwitchHandleForcedSolve()
     {
-        int start = solution.Length - (unpressedbuttons.Count() - 1);
+        int start = solution.Length - (unpressedButtons.Count() - 1);
         for (int i = start; i < solution.Length; i++)
         {
             buttons[solution[i].pos].OnInteract();
